fb41dad8110924f1221775275cfbbe496cf02225
Deepakraja03 <12306deepakraja@gmail.com>
2025-04-05 08:22:43 +0800

    home updated

--- components/layouts/site-header.tsx
+++ components/layouts/site-header.tsx
@@ -9,7 +9,7 @@ import { MobileNav } from "./mobile-nav";
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background rounded-t-2xl backdrop-blur-sm">
-      <div className="flex h-16 items-center justify-between w-full px-5 md:px-10 lg:px-20">
+      <div className="flex h-16 items-center justify-between w-full px-5 md:px-10 lg:px-10">
         <Logo />
         <MainNav />
         <MobileNav />
